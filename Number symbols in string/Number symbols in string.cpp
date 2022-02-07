72f24a7c316df393e7cdcdbc1f37c2301d1421cc
Саша <@DESKTOP-KJAD6UE>
2022-02-07 21:02:37 +0200

    Add project files.

---
+++ Number symbols in string/Number symbols in string.cpp
@@ -0,0 +1,19 @@
+﻿// Number symbols in string.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <conio.h>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	string a;
+	getline(cin, a);//введення рядка
+	int size = a.size();
+	
+	cout << "Кількість літер, цифр та символів у стрічці дорівнює - " << size <<endl;	
+}
